1c9256905ee3fc0ce90fae137eb31dcf0005fa31
Kamil Kosiba <kamil.kosiba@outlook.com>
2018-07-10 12:07:08 +0100

    Added post paths

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('posts/<int:year>/', views.years_archive),
+    path('posts/<int:year>/<int:month>/', views.month_archive),
+    path('posts/<int:year>/<int:month>/<int:key>/', views.post_detail),
     path('', views.index, name='index'),
 ]
