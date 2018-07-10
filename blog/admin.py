d382b0065d75110415163fa5beabc05bf1cb6c91
Kamil Kosiba <kamil.kosiba@outlook.com>
2018-07-10 18:29:51 +0100

    Updated registers

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Post, Author
+from .models import Author, Post, Comment
 
 # Register your models here.
 admin.site.register(Author)
 admin.site.register(Post)
+admin.site.register(Comment)
