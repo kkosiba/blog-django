8e143d01791eeec52f6330fbf41e99541b294dbf
Kamil Kosiba <kamil.kosiba@outlook.com>
2018-07-16 12:27:31 +0100

    Updated url routes for login/logout

--- website/urls.py
+++ website/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('blog.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
 ]
