600da31831f855f81f40f39031c4cf73d272478f
Kamil Kosiba <kamil.kosiba@outlook.com>
2018-07-17 12:49:16 +0100

    Replaced Author model with built-in User model

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Author, Post, Comment
+
+from .models import Post
+from .models import Comment
 
 # Register your models here.
-admin.site.register(Author)
 admin.site.register(Post)
 admin.site.register(Comment)
