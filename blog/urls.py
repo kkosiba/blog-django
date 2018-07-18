9511b1b0d14a65ec2c9b47d2a56d158eb4ee94b0
Kamil Kosiba <kamil.kosiba@outlook.com>
2018-07-18 23:17:59 +0100

    Updated url paths

--- blog/urls.py
+++ blog/urls.py
@@ -3,12 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('<int:year>/', views.years_archive),
-    # path('<int:year>/<int:month>/', views.month_archive),
-    # path('<int:year>/<int:month>/<int:day>/', views.day_archive),
+    path('signup/', views.signup, name='signup'),
     path('post/add/', views.add, name='add'),
     path('contact/', views.contact, name='contact'),
-    path('archive/', views.archive, name='archive'),
     path('about/', views.about, name='about'),
     path('', views.index, name='index'),
 ]
