e41973a12346555e2d01764a9d232223fc2a9047
Kamil Kosiba <kamil.kosiba@outlook.com>
2018-07-19 19:57:31 +0100

    Added Category to admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
+from .models import Category
 from .models import Post
 from .models import Comment
 
 # Register your models here.
+admin.site.register(Category)
 admin.site.register(Post)
 admin.site.register(Comment)
