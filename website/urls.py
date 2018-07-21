e8799262ada9a2ac6e8861631d7dbce96f83953b
Kamil Kosiba <kamil.kosiba@outlook.com>
2018-07-21 20:24:16 +0100

    Restructured urls

--- website/urls.py
+++ website/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('', include('blog.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
 ]
