5a0f734498cbe516d2a75435259fe51070fe75f7
Kamil Kosiba <kamil.kosiba@outlook.com>
2018-07-24 18:30:11 +0100

    Organized and cleaned paths

--- blog/urls.py
+++ blog/urls.py
@@ -5,22 +5,25 @@ from blog.feeds import LastEntriesFeed
 from . import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
+
+    # user management
     path('login/', auth_views.login, name='login'),
     path('logout/', auth_views.logout, name='logout'),
     path('signup/', views.signup, name='signup'),
-    path('search/', views.search, name='search'),
-    path('category 1/', views.category1, name='category1'),
-    path('category 2/', views.category2, name='category2'),
-    path('category 3/', views.category3, name='category3'),
-    path('category 4/', views.category4, name='category4'),
-    path('category 5/', views.category5, name='category5'),
-    path('add/', views.add, name='add'),
-    path('contact/', views.contact, name='contact'),
+
+    # posts related
+    path('post/<slug:slug>/', views.single_post, name='single_post'),
+    path('posts/<int:year>/', views.year, name='year'),
+    path('posts/<int:year>/<int:month>/', views.year_month, name='year_month'),
+    path('posts/category 1/', views.category1, name='Category 1'),
+    path('posts/category 2/', views.category2, name='Category 2'),
+    path('posts/category 3/', views.category3, name='Category 3'),
+    path('posts/add/', views.add, name='add'),
+    path('posts/search/', views.search, name='search'),
+
+    # misc
     path('about/', views.about, name='about'),
-    path('', views.index, name='index'),
-    path('archive/<int:year>/<int:month>/', views.year_month, name='year_month'),
-    path('archive/<int:year>/', views.year, name='year'),
-    path('post/<int:pk>/', views.single_post, name='single_post'),
-    # path('post/<int:pk>/add_comment/', views.add_comment, name='add_comment'),
+    path('contact/', views.contact, name='contact'),
     path('latest/feed/', LastEntriesFeed()),
 ]
