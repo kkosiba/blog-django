a083eff00839bbb9410d859bfafb8757f6836b8f
Kamil Kosiba <kamil.kosiba@outlook.com>
2018-07-24 18:26:46 +0100

    Extended and improved existing models

--- blog/models.py
+++ blog/models.py
@@ -6,78 +6,55 @@ from django.utils import timezone
 # Create your models here.
 
 class Category(models.Model):
-    """
-    Django database model for categories of Posts
-    """
-
     name = models.CharField(max_length=100, unique=True)
 
     class Meta:
-        verbose_name_plural = 'Categories'  # to have 'Categories' in admin panel
+        verbose_name_plural = 'Categories'
 
     def __str__(self):
         return self.name
 
 
 class Post(models.Model):
-    """
-    Django database model for posts
-    """
+    CHOICES = (
+        ('draft', 'Draft'),
+        ('submitted', 'Submitted'), )
+
+    class Meta:
+        ordering = ('-published_date', )
 
     # posts' category (many-to-many)
     category = models.ManyToManyField(Category, blank=False)
 
     # author (many Posts to one User)
     # only Users can be post authors
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-
-    # # comments
-    # comments = models.ForeignKey(Comment, on_delete=models.CASCADE)
-
-    # title
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
     title = models.CharField(max_length=300)
-
-    # post content
+    slug = models.SlugField(max_length=300, unique_for_date='published_date')
     content = models.TextField()
 
-    # tags
-    # tags = models.ManyToManyField(Tag)
+    published_date = models.DateTimeField(default=timezone.now)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10, choices=CHOICES, default='draft')
 
-    # date of publication
-    published_date = models.DateTimeField(blank=True, null=True)
-
-    # string representation returning title of a post
     def __str__(self):
         return self.title
 
 
 class Comment(models.Model):
-    """
-    Django database model for comment feature
-    """
+    CHOICES = (
+        ('rejected', 'Rejected'),
+        ('accepted', 'Accepted'), )
 
-    # author of a comment
-    # author = models.CharField(max_length=70)
+    # bind comment to author and post
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
 
-    # associate comment with a post
-    post = models.ForeignKey(
-        Post, on_delete=models.CASCADE, related_name='comments')
-
-    # comment's content (no longer than 500 symbols)
     content = models.TextField(max_length=500)
 
-    # date of comment creation
-    created = models.DateTimeField(default=timezone.now)
+    created_date = models.DateTimeField(default=timezone.now)
+    status = models.CharField(max_length=10, choices=CHOICES, default='rejected')
 
-    # approved?
-    approved = models.BooleanField(default=False)
-
-    # approve comment
-    def approve(self):
-        self.approved = True
-        self.save()
-
-    # string representation returning comment's content
     def __str__(self):
         return self.content
