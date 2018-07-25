80bf760959e5b04055d20204cee878f9fcc854f2
Kamil Kosiba <kamil.kosiba@outlook.com>
2018-07-25 20:00:53 +0100

    Updated paths with CBVs

--- blog/urls.py
+++ blog/urls.py
@@ -2,24 +2,32 @@ from django.contrib.auth import views as auth_views
 from django.urls import path
 from blog.feeds import LastEntriesFeed
 
+# for restricting access to post related actions
+from django.contrib.auth.decorators import login_required
+
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-
     # user management
     path('login/', auth_views.login, name='login'),
     path('logout/', auth_views.logout, name='logout'),
     path('signup/', views.signup, name='signup'),
 
     # posts related
-    path('post/<slug:slug>/', views.single_post, name='single_post'),
-    path('posts/<int:year>/', views.year, name='year'),
-    path('posts/<int:year>/<int:month>/', views.year_month, name='year_month'),
-    path('posts/category 1/', views.category1, name='Category 1'),
-    path('posts/category 2/', views.category2, name='Category 2'),
-    path('posts/category 3/', views.category3, name='Category 3'),
-    path('posts/add/', views.add, name='add'),
+    path('', views.ListPostsView.as_view(), name='index'),
+    path('post/<slug:slug>/',
+         views.DetailsPostView.as_view(), name='single_post'),
+    path('posts/<int:year>/',
+         views.ListPostsByYearView, name='year'),
+    path('posts/<int:year>/<int:month>/',
+         views.ListPostsByYearMonthView.as_view(), name='year_month'),
+    path('posts/category/<str:name>/', views.category, name='category'),
+    path('posts/add/',
+         login_required(views.AddPostView.as_view()), name='add_post'),
+    path('posts/<slug:slug>/delete/',
+         login_required(views.DeletePostView.as_view()), name='delete_post'),
+    path('posts/<slug:slug>/update/',
+         login_required(views.UpdatePostView.as_view()), name='update_post'),
     path('posts/search/', views.search, name='search'),
 
     # misc
