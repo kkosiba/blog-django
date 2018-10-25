7471af242247ff33e05a3c4568679f9e1a5a984d
Kamil Kosiba <kamil.kosiba@outlook.com>
2018-10-25 20:42:55 +0100

    CHANGELOG
    1. Replaced PostAPIView with suitable ViewSet
    2. Added ViewSet for handling users

--- blog/api/views.py
+++ blog/api/views.py
@@ -1,7 +1,56 @@
-from rest_framework.generics import ListAPIView
-from blog.models import Post
-from .serializers import PostSerializer
+from rest_framework import generics, permissions
+from .permissions import IsOwnerOrReadOnly
 
-class PostAPIView(ListAPIView):
+from blog.models import Post
+from django.contrib.auth.models import User
+from .serializers import PostSerializer, UserSerializer
+
+from rest_framework import viewsets
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+
+
+from django.shortcuts import get_object_or_404
+
+# for later use
+# class MultipleFieldLookupMixin(object):
+#     """
+#     Apply this mixin to any view or viewset to get multiple field filtering
+#     based on a `lookup_fields` attribute, instead of the default single field filtering.
+#     """
+#     def get_object(self):
+#         queryset = self.get_queryset()             # Get the base queryset
+#         queryset = self.filter_queryset(queryset)  # Apply any filter backends
+#         filter = {}
+#         for field in self.lookup_fields:
+#             if self.kwargs[field]: # Ignore empty fields.
+#                 filter[field] = self.kwargs[field]
+#         obj = get_object_or_404(queryset, **filter)  # Lookup the object
+#         self.check_object_permissions(self.request, obj)
+#         return obj
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    """
+    This viewset automatically provides `list`, `create`, `retrieve`,
+    `update` and `destroy` actions.
+    """
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly, )
+
+
+    # doesn't work yet...
+    # def perform_create(self, serializer):
+    #     serializer.save(author__first_name=self.request.user.first_name)
+
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+
