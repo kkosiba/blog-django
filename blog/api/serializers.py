3fc3231948fe0311bf146fae6cc46d0f8b182f1e
Kamil Kosiba <kamil.kosiba@outlook.com>
2018-10-25 20:40:12 +0100

    Added UserSerializer class

--- blog/api/serializers.py
+++ blog/api/serializers.py
@@ -1,13 +1,23 @@
 from rest_framework.serializers import ModelSerializer
+
 from blog.models import Post
+from django.contrib.auth.models import User
+
 
 class PostSerializer(ModelSerializer):
     class Meta:
         model = Post
         fields = (
+            'id',
             'category',
             'author',
             'slug',
             'content',
             'published_date',
             )
+
+
+class UserSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'first_name', )
