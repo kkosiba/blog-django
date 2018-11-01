142897348b8bbf204274a51e376109c5e2703b0f
Kamil Kosiba <kamil.kosiba@outlook.com>
2018-11-01 20:05:13 +0000

    CHANGELOG
    1. Removed 'id' field from UserSerializer
    2. Added CategorySerializer class to nicely display categories in posts API
    3. Updated PostSerializer:
        - added hyperlinked field url to post details
        - added 'author' through UserSerializer (to display authors' name
          instead of number
        - removed 'id', 'slug' fields

--- blog/api/serializers.py
+++ blog/api/serializers.py
@@ -1,23 +1,37 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import (
+    ModelSerializer,
+    HyperlinkedIdentityField,
+    )
 
-from blog.models import Post
+from blog.models import Post, Category
 from django.contrib.auth.models import User
-
-
-class PostSerializer(ModelSerializer):
-    class Meta:
-        model = Post
-        fields = (
-            'id',
-            'category',
-            'author',
-            'slug',
-            'content',
-            'published_date',
-            )
-
 
 class UserSerializer(ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'first_name', )
+        fields = ('first_name', )
+
+
+class CategorySerializer(ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ('name', )
+
+
+class PostSerializer(ModelSerializer):
+    url = HyperlinkedIdentityField(
+        view_name='post-detail',
+        lookup_field='slug',
+        )
+    author = UserSerializer()
+    category = CategorySerializer(many=True)
+
+    class Meta:
+        model = Post
+        fields = (
+            'url',
+            'author',
+            'category',
+            'content',
+            'published_date',
+            )
