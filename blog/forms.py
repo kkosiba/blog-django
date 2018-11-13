b0ca0d05b5d33ba164033698e68624b11d9f20e0
Kamil Kosiba <kamil.kosiba@outlook.com>
2018-11-13 21:59:09 +0000

    CHANGELOG
    1. Commented out old code related to account management and profiles.
    2. Added function make_slug(), generating unique slug at post creation/update

--- blog/forms.py
+++ blog/forms.py
@@ -3,71 +3,88 @@ from django import forms
 from django.forms import (
     ModelForm, Textarea, TextInput, EmailField,
     EmailInput, SelectMultiple, CharField,
-    PasswordInput, ClearableFileInput,
+    PasswordInput, ClearableFileInput, Select,
+    CheckboxInput,
     )
 
 from django.utils.text import slugify
+from django.utils.crypto import get_random_string
 
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
-from .models import Category, Post, Profile
+from .models import Category, Post
 
 from django.core.exceptions import ValidationError
 
 
-class UserForm(ModelForm):
-    class Meta:
-        model = User
-        fields = ('first_name', 'last_name', 'email', )
+# class UserForm(ModelForm):
+#     class Meta:
+#         model = User
+#         fields = ('first_name', 'last_name', 'email', )
 
 
-class ProfileForm(ModelForm):
-    class Meta:
-        model = Profile
-        fields = ('avatar', 'bio', )
+# class ProfileForm(ModelForm):
+#     class Meta:
+#         model = Profile
+#         fields = ('avatar', 'bio', )
 
 
-class SignUpForm(UserCreationForm):
-    username = CharField(label='Username',
-        widget=TextInput(
-            attrs={'class': 'form-control',
-                   'required': True,
-                   }))
-    email = EmailField(label='Email',
-        widget=EmailInput(
-            attrs={'class': 'form-control',
-                   'required': True,
-                   }))
-    password1 = CharField(label='Password',
-        widget=PasswordInput(
-            attrs={'class': 'form-control',
-                   'required': True,
-                   }))
-    password2 = CharField(label='Confirm password',
-        widget=PasswordInput(
-            attrs={'class': 'form-control',
-                   'required': True,
-                   }))
+# class SignUpForm(UserCreationForm):
+#     username = CharField(label='Username',
+#         widget=TextInput(
+#             attrs={'class': 'form-control',
+#                    'required': True,
+#                    }))
+#     email = EmailField(label='Email',
+#         widget=EmailInput(
+#             attrs={'class': 'form-control',
+#                    'required': True,
+#                    }))
+#     password1 = CharField(label='Password',
+#         widget=PasswordInput(
+#             attrs={'class': 'form-control',
+#                    'required': True,
+#                    }))
+#     password2 = CharField(label='Confirm password',
+#         widget=PasswordInput(
+#             attrs={'class': 'form-control',
+#                    'required': True,
+#                    }))
 
-    class Meta:
-        model = User
-        fields = ('username', 'email', 'password1', 'password2', )
+#     class Meta:
+#         model = User
+#         fields = ('username', 'email', 'password1', 'password2', )
 
-    def clean_username(self):
-        username = self.cleaned_data.get('username')
-        qs = User.objects.filter(username=username)
-        if qs.exists():
-            raise ValidationError("Username already in use!")
-        return username
+#     def clean_username(self):
+#         username = self.cleaned_data.get('username')
+#         qs = User.objects.filter(username=username)
+#         if qs.exists():
+#             raise ValidationError("Username already in use!")
+#         return username
 
-    def save(self, commit=True):
-        instance = super().save(commit=False)
-        instance.first_name = self.cleaned_data.get('username', '').capitalize()
-        instance.email = self.cleaned_data.get('email', '')
-        if commit:
-            instance.save()
-        return instance
+#     def save(self, commit=True):
+#         instance = super().save(commit=False)
+#         instance.first_name = self.cleaned_data.get('username', '').capitalize()
+#         instance.email = self.cleaned_data.get('email', '')
+#         if commit:
+#             instance.save()
+#         return instance
+
+def make_slug(instance, new_slug=None):
+    """
+    Function for creating unique slugs
+    """
+
+    slug = slugify(instance.title)
+    if new_slug is not None:
+        slug = new_slug
+    # check if there exists a post with existing slug
+    q = Post.objects.filter(slug=slug)
+    if q.exists():
+        new_slug = '-'.join([slug, get_random_string(4,'0123456789')])
+        return make_slug(instance, new_slug=new_slug)
+    return slug
 
 
 class AddPostForm(ModelForm):
@@ -78,8 +95,20 @@ class AddPostForm(ModelForm):
             'title': TextInput(
                 attrs={
                     'required': True,
-                    'placeholder': 'Type your title here...', }, ),
-            'category': SelectMultiple(attrs={'required': True, }, ),
+                    'placeholder': 'Type your title here...',
+                    'class': 'form-control', }, ),
+            'category': SelectMultiple(
+                attrs={
+                    'required': True, 
+                    'class': 'form-control', }, ),
+            'status': Select(
+                attrs={
+                    'required': True, 
+                    'class': 'form-control', }, ),
+            'allow_comments': CheckboxInput(
+                attrs={
+                    'required': True, 
+                    'class': 'form-control', }, ),
         }
 
     def clean(self):
@@ -94,7 +123,7 @@ class AddPostForm(ModelForm):
 
     def save(self, commit=True):
         instance = super().save(commit=False)
-        instance.slug = slugify(self.cleaned_data.get('title', ''))
+        instance.slug = make_slug(instance)
 
         if commit:
             instance.save()
