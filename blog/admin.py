b4540d34da4486cf2523c4be729576c851d7c0f0
Kamil Kosiba <kamil.kosiba@outlook.com>
2018-11-13 21:51:15 +0000

    Removed Profile from admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
 
-from .models import Category, Post, Profile
+from .models import Category, Post
 
 # Register your models here.
 admin.site.register(Category)
 admin.site.register(Post)
-admin.site.register(Profile)
