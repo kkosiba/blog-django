dff4c7fd28466d5b86c5bc977d9d873d6d9b5abd
Kamil Kosiba <kamil.kosiba@outlook.com>
2019-05-26 18:56:50 +0100

    Updated urls to accommodate app change blog->posts

--- website/urls.py
+++ website/urls.py
@@ -1,36 +1,17 @@
-"""website URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
-
 from django.conf.urls.static import static
 from django.conf import settings
-
-from blog.feeds import LastEntriesFeed
+from posts.feeds import LastEntriesFeed
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls', namespace='accounts')),
-
-    path('contact/', include('contact.urls', namespace='contact')),
-    path('latest/feed/', LastEntriesFeed(), name='feed'),
-   
-    path('api/', include('blog.api.urls')), # REST api
-    path('', include('blog.urls')),
-    path('markdownx/', include('markdownx.urls')),
+    path("admin/", admin.site.urls),
+    path("accounts/", include("accounts.urls", namespace="accounts")),
+    path("contact/", include("contact.urls", namespace="contact")),
+    path("latest/feed/", LastEntriesFeed(), name="feed"),
+    path("api/", include("posts.api.urls")),  # REST api
+    path("", include("posts.urls")),
+    path("markdownx/", include("markdownx.urls")),
 ]
 
 # to load static/media files in development environment
