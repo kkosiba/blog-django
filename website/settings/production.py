d6359eeac7b4242c0ce323b3bdf5246cba1e7e4b
Kamil Kosiba <kamil.kosiba@outlook.com>
2019-05-27 12:22:12 +0100

    Cleanup

--- website/settings/production.py
+++ website/settings/production.py
@@ -3,7 +3,6 @@
 from .base import *
 
 import django_heroku
-# import whitenoise
 
 SECRET_KEY = os.environ.get("SECRET_KEY", "")
 
@@ -12,9 +11,6 @@ DEBUG = True
 ALLOWED_HOSTS = ["https://django-blog-1k4z.herokuapp.com/"]
 
 
-# MIDDLEWARE += ["whitenoise.middleware.WhiteNoiseMiddleware"]
-
-# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = "/static/"
 # STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
