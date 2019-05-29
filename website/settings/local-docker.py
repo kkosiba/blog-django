04552d8ca7bb12460065a08cd6d406363f5f5626
Kamil Kosiba <kamil.kosiba@outlook.com>
2019-05-29 14:40:31 +0100

    Added custom Docker settings

---
+++ website/settings/local-docker.py
@@ -0,0 +1,34 @@
+# Development settings - for Docker
+
+from .base import *
+
+SECRET_KEY = "0hgq)139a$a!9+$gh^y2#qdtrl)h#lf*u&5(uwz@#82^)-hrbz"
+
+DEBUG = True
+
+ALLOWED_HOSTS = ["0.0.0.0", "127.0.0.1"]
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "HOST": "db",
+        "PORT": "5432",
+    }
+}
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+DEFAULT_FROM_EMAIL = "admin@localhost"
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ""
+EMAIL_USE_TLS = False
+EMAIL_PORT = 1025
+
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
+
+# Media files
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
